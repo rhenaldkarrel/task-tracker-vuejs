4e94537b3bad6a103610a6468458df78784144df
Rhenald <renaldkarel@gmail.com>
2023-03-24 14:27:13 +0700

    chore: adjust about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,13 @@
 <template>
-  <h3>Version 1.0.0</h3>
-  <router-link to="/">Go Back</router-link>
+	<h3>Version 1.0.0</h3>
+	<p>
+		This is a Task Tracker app created by
+		<a
+      href="https://github.com/rhenaldkarrel"
+      target="_blank"
+		>
+    Rhenald Karrel
+    </a>
+	</p>
+	<router-link to="/">Go Back</router-link>
 </template>
